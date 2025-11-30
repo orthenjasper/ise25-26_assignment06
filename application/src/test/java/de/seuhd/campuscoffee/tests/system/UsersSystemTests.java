01681f54d4826dba8e27360f501fe9f4b9e83bb8
Jasper Cedric Orthen <jasper.orthen@stud.uni-heidelberg.de>
2025-11-30 22:35:55 +0100

    Implement User controller

--- application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
@@ -1,16 +1,26 @@
 package de.seuhd.campuscoffee.tests.system;
 
+import de.seuhd.campuscoffee.domain.model.User;
+import de.seuhd.campuscoffee.domain.tests.TestFixtures;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+//import org.springframework.http.HttpStatus;
+
+import static de.seuhd.campuscoffee.tests.SystemTestUtils.Requests.userRequests;
+//import static org.assertj.core.api.Assertions.assertThat;
+
 public class UsersSystemTests extends AbstractSysTest {
 
-    //TODO: Uncomment once user endpoint is implemented
+    //completed: Uncomment once user endpoint is implemented
 
-//    @Test
-//    void createUser() {
-//        User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
-//        User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
-//
-//        assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
-//    }
+   @Test
+   void createUser() {
+       User userToCreate = TestFixtures.getUserListForInsertion().getFirst();
+       User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
+
+       assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
+   }
 
     //TODO: Add at least two additional tests for user operations
 
